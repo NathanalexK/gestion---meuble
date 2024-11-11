ed8ff1aca7d44feefbec70759e4bec79476db5e1
Tsinjoniaina18 <tsinjoniaina.randriamirado@gmail.com>
2024-11-11 10:25:41 +0300

    Contrat d'essais

--- src/main/java/com/source/meuble/talent/entretient/EntretientRepository.java
+++
@@ -1,9 +0,0 @@
-package com.source.meuble.talent.entretient;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface EntretientRepository extends JpaRepository<Entretient, Integer> {
-
-}
